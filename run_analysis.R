45665bfdf65778fb0550fee6ce0cce010d7b18a1
William Karpinski <bkarpi@cox.net>
2014-07-25 18:39:09 -0700

    updated r script

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,4 @@
-##This R script requires the reshape 2 package
-
-## Part1 - Read the training and test datasets into dataframes  
+### Part1 - Read the training and test datasets into dataframes  
 trainData    <- read.table("./train/X_train.txt")
 testData     <- read.table("./test/X_test.txt")
 combData     <- rbind(trainData, testData)
@@ -14,14 +12,14 @@ testSubject  <- read.table("./test/subject_test.txt")
 combSubject  <- rbind(trainSubject, testSubject)
 
 
-## Part2 Extracts only the measurements on the mean and standard deviation for each measurement
+### Part2 Extracts only the measurements on the mean and standard deviation for each measurement
 features <- read.table("./features.txt")
 keepCols <- grep("mean\\(\\)|std\\(\\)", features[, 2])  #identify cols with STD or Mean
 
 combData <- combData[, keepCols] #only keep columns identified as std or mean
 
 
-## Part3 Uses descriptive activity names to name the activities in the dataset
+### Part3 Uses descriptive activity names to name the activities in the dataset
 activity <- read.table("./activity_labels.txt")
 
 activityLabel <- activity[combLabels[, 1], 2]
@@ -30,7 +28,7 @@ combLabels[, 1] <- activityLabel           #merge combLabels col1 with activity
 
 names(combLabels) <- "activity"            #label column activity
 
-## Part4 Appropriately labels the data set with descriptive variable names
+### Part4 Appropriately labels the data set with descriptive variable names and combines all data
 features[,2] <- gsub('[-()]', '', features[,2])     
 features[,2] <- gsub('mean', 'Mean', features[,2])   
 features[,2] <- gsub('std', 'Std', features[,2])
@@ -40,8 +38,8 @@ names(combSubject) <- "subject"
 
 tidyData1 <- cbind(combSubject, combLabels, combData)
  
-## Part5 Creates a second, independent tidy data set with the mean of each variable
-## for each activity and each subject.
+### Part5 Creates a second, independent tidy data set with the mean of each variable
+### for each activity and each subject.
 meltdata <- melt(tidyData1, id=c("subject","activity"))
 tidyData2 <- dcast(meltdata, subject+activity ~ variable, mean)
 
